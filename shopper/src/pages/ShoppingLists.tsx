6e6942d5d30357f1816d6d698e6d65d05d7957d3
MaciekMakowski <mvciek.mvkowski@gmail.com>
2024-08-11 18:06:59 +0200

    Add export functionality to ShoppingLists page

--- shopper/src/pages/ShoppingLists.tsx
+++ shopper/src/pages/ShoppingLists.tsx
@@ -1,7 +1,7 @@
+import ExportIcon from "@/assets/icons/export.svg?react";
 import { getAllShoppingLists } from "@/idb/shoppingListController";
 import { useEffect, useState } from "react";
-import { ShoppingList } from "../interfaces/shoppingList";
-
+import { ProductInList, ShoppingList } from "../interfaces/shoppingList";
 const ShoppingLists = () => {
   const [shoppingLists, setShoppingLists] = useState<ShoppingList[]>([]);
 
@@ -15,6 +15,14 @@ const ShoppingLists = () => {
     handleGetShoppingLists();
   }, []);
 
+  const exportToStorage = (products: ProductInList[]) => {
+    const listFromProductsAsText = products
+      .map((product) => `${product.product.name} - ${product.quantity}`)
+      .join("\n");
+
+    navigator.clipboard.writeText(listFromProductsAsText);
+  };
+
   return (
     <div className="flex flex-col gap-4 p-4 w-full">
       <h1 className="text-3xl font-bold font-secondary">Shopping Lists</h1>
@@ -25,7 +33,15 @@ const ShoppingLists = () => {
             key={shoppingList.id}
             className="p-4 bg-white shadow-md rounded-md flex gap-2 flex-col"
           >
-            <span className="font-semibold text-xl">{shoppingList.name}</span>
+            <div className="flex justify-between">
+              <span className="font-semibold text-xl">{shoppingList.name}</span>
+              <ExportIcon
+                className="cursor-pointer"
+                width={24}
+                height={24}
+                onClick={() => exportToStorage(shoppingList.products)}
+              />
+            </div>
             <div className="flex gap-2 items-end">
               <span>{shoppingList.shop.name}</span>
               <span className="text-sm">{shoppingList.date}</span>
